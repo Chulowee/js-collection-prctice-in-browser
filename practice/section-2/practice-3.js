cb269460c1ded2a7411dfeceba3ea5aff2cad13c
unknown <katrina.chloe.reyes@oocl.com>
2019-09-15 21:27:22 +0800

    Answer for Section 2, practice 3

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,39 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var sameElements = [];
+  collection.forEach(element => {
+    let splitElement = separateElement(element);
+    let elementExists = sameElements.find(a => splitElement.name === a.name);
+  
+    if (elementExists) {
+      elementExists.summary += splitElement.summary;
+   } else{
+    sameElements.push(splitElement); 
+   }
+  });
+
+  function separateElement(element){  
+    let lmnt = element;
+    let cnt = 1;
+    if (element.includes("-")){
+      var splt = element.split("-");
+      lmnt = splt[0];
+      cnt = splt[1];
+    } else if (element.includes(":")) {
+      var splt = element.split(":");
+      lmnt = splt[0];
+      cnt = splt[1];
+    } else if (element.includes("[")) {
+      var splt = element.split("[");
+      lmnt = splt[0];
+      cnt = splt[1];
+    } else if (element.includes("]")) {
+      var splt = element.split("]");
+      lmnt = splt[0];
+      cnt = splt[1];
+    }
+ return({ name: lmnt, summary: parseInt(cnt) });
+  }
+  return sameElements;
 }
