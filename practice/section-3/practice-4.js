41da0cca8984c6695d6901a770c13ee4e5a6df0d
unknown <katrina.chloe.reyes@oocl.com>
2019-09-15 21:30:10 +0800

    Answer for Section 3, practice 4

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,35 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let collectionC =[];
+
+  collectionA.forEach(element => {
+    let splitElement = separateElement(element);
+    let elementExists = collectionC.find(a => splitElement.key === a.key);
+    if (elementExists) {
+      elementExists.count++;
+   } else {
+    collectionC.push(separateElement(element)); }
+  });
+
+  collectionC.forEach(elements => {
+    if (objectB.value.includes(elements.key)) {
+         if (elements.count/3){
+          elements.count -= parseInt(elements.count/3);
+         }
+    }
+});
+  
+function separateElement(element){  
+  let lmnt = element;
+  let cnt = 1;
+  if (element.includes("-")){
+    var splt = element.split("-");
+    lmnt = splt[0];
+    cnt = splt[1];
+  } 
+return({ key: lmnt, count: parseInt(cnt) });
+}
+
+  return collectionC;
 }
